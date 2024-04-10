1fb963b8ac9bdcfd09d1d31bd7248b0cb16495b0
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-04-10 16:24:41 +0200

    connect transpot-payment component with BE

--- src/app/transpot-payment/transpot-payment.component.ts
+++ src/app/transpot-payment/transpot-payment.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { map, Observable } from "rxjs";
+import { TextItem } from "../model/view";
+import { SingleTypesService } from "../apollo/single-types.service";
 
 @Component({
   selector: 'app-transpot-payment',
   templateUrl: './transpot-payment.component.html',
-  styleUrl: './transpot-payment.component.scss'
 })
 export class TranspotPaymentComponent {
 
+  payment$: Observable<TextItem[]> =
+    this.singleTypesService.getPaymentData<{paymentMethods: TextItem[]}>()
+      .pipe(
+        map(d => d.data.paymentMethods));
+
+
+  transport$: Observable<TextItem[]> =
+    this.singleTypesService.getTransportData<{transportMethods: TextItem[]}>()
+      .pipe(
+        map(d => d.data.transportMethods));
+
+  constructor(
+    private singleTypesService: SingleTypesService,
+  ) {}
 }
