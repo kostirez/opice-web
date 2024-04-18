e09c4764795f7d9d92f5659bb43632ad2e89d269
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-04-18 22:19:19 +0200

    fix capital letter and remove scss file

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,7 +4,6 @@ import { DOCUMENT } from "@angular/common";
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
