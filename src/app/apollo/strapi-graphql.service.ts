fd601d569847c51ff6efbeec26fa8ff12544a78d
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-09-03 10:06:27 +0200

    add dynamic types to graphql service

--- src/app/apollo/strapi-graphql.service.ts
+++ src/app/apollo/strapi-graphql.service.ts
@@ -11,6 +11,16 @@ interface SingleDataStructure {
   };
 }
 
+export interface OnDynamic {
+  compName: string;
+  params: (string | ComponentParams | QueryParams)[];
+}
+
+export interface DynamicParams{
+  dynamicName: string;
+  on: OnDynamic[];
+}
+
 export interface ComponentParams {
   compName: string,
   params: (string | QueryParams)[]
@@ -18,14 +28,15 @@ export interface ComponentParams {
 
 export interface QueryParams {
   colection: string;
-  filtr: string;
-  items: (string | QueryParams | ComponentParams)[];
+  filter: string;
+  items: (string | QueryParams | ComponentParams| DynamicParams)[];
 }
 
 
 
 export interface StrapiResult<DATA> {
   data: DATA;
+  meta?: any
   loading: boolean;
 }
 
@@ -38,8 +49,8 @@ export class StrapiGraphqlService {
 
   //////////////////////////////////
 
-  fetch<T>(queryParams: QueryParams): Observable<StrapiResult<T>> {
-    const q = this.getSimpleQuery(queryParams)
+  fetch<T>(queryParams: QueryParams, includeMeta: boolean = false): Observable<StrapiResult<T>> {
+    const q = this.getSimpleQuery(queryParams, includeMeta)
     const query = gql`
       query Query {
         ${q}
@@ -51,27 +62,36 @@ export class StrapiGraphqlService {
         map(r =>{
           return {
             ...r,
-            data: this.mapSimpleResult(
+            ...this.mapSimpleResult(
               queryParams, r
-            )
+            ),
           }
         })
       )
   }
 
-  getSimpleQuery(query: QueryParams): string {
+  getSimpleQuery(query: QueryParams, includeMeta: boolean = false): string {
+    const meta =`
+      meta {
+        pagination {
+          total
+          pageCount
+          page
+        }
+      }`;
     return `
-      ${query.colection}${query.filtr } {
+      ${query.colection}${query.filter } {
         data {
           id
           attributes {
             ${this.getQueryItems(query.items)}
           }
         }
+        ${includeMeta ? meta : ''}
       }`
   }
 
-  private getQueryItems(items: (string | QueryParams | ComponentParams)[]):string {
+  private getQueryItems(items: (string | QueryParams | ComponentParams | DynamicParams)[]):string {
     let ret = '';
     items.forEach(item => {
       if (typeof item === "string") {
@@ -80,47 +100,81 @@ export class StrapiGraphqlService {
         if("compName" in item && item.compName) {
           ret += item.compName + '{' + this.getQueryItems(item.params) + '}';
         } else {
-          ret += this.getSimpleQuery(item as QueryParams)
+          if ("dynamicName" in item && item.dynamicName){
+            ret += item.dynamicName + '{'
+            item.on.forEach(dynamicItem => {
+              ret += '... on ' + dynamicItem.compName + '{';
+              ret += this.getQueryItems(dynamicItem.params) + '}';
+            })
+            ret += '}';
+          } else {
+            ret += this.getSimpleQuery(item as QueryParams)
+          }
         }
       }
     });
     return ret;
   }
 
-  mapSimpleResult<T>(query: QueryParams, result: StrapiResult<T>): any {
+  mapSimpleResult<T>(query: QueryParams, result: StrapiResult<T>): {data: any, meta: any} {
     return this.mapColection(query, result.data[query.colection])
   }
 
-  mapColection(query: QueryParams, data: any): any {
+  mapColection(query: QueryParams, data: any): {data: any, meta: any} {
+    const meta = data.meta
     if(Array.isArray(data.data)) {
       const arr = [];
       data.data.forEach(item => {
         arr.push(this.mapResultItems(query.items, item.attributes, item.id))
       })
-      return arr
+      return {data: arr, meta}
     } else {
       const d = data.data.attributes;
-     return this.mapResultItems(query.items, d, data.data.id)
+      return {
+        data: this.mapResultItems(query.items, d, data.data.id),
+        meta
+      }
     }
   }
 
-  mapResultItems(items: (string | QueryParams | ComponentParams)[], data: {}, id?: any): any {
-    let ret = {id: id};
+  mapDynamicZone(on: OnDynamic, data: any) {
+    return this.mapResultItems(on.params, data)
+  }
 
+  mapResultItems(items: (string | QueryParams | ComponentParams | DynamicParams)[], data: {}, id?: any): any {
+    let ret = {id: id};
     items.forEach(item => {
       if (typeof item === "string") {
         ret[item] = data[item]
       } else {
         if ("compName" in item && "params" in item && item.compName) {
-          const arr = [];
           item = item as ComponentParams;
-          for (let d of data[item.compName]) {
-            arr.push(this.mapResultItems(item.params, d))
+          if (Array.isArray(data[item.compName])) {
+            const arr = [];
+            for (let d of data[item.compName]) {
+              arr.push(this.mapResultItems(item.params, d));
+            }
+            ret[item.compName]= arr;
+
+          } else {
+            ret[item.compName] = this.mapResultItems(item.params, data[item.compName]);
           }
-          ret[item.compName]= arr;
         } else {
-          item = item as QueryParams;
-          ret[item.colection] = this.mapColection(item, data[item.colection])
+          if ("dynamicName" in item && item.dynamicName) {
+            item = item as DynamicParams;
+            const name = item.dynamicName;
+            const dynamicArr = [];
+            data[name].forEach((dynamicItem) => {
+              const componentDef = (item as DynamicParams).on.find(def => def.compName== dynamicItem.__typename);
+              if (componentDef) {
+                dynamicArr.push(this.mapDynamicZone(componentDef, dynamicItem))
+              }
+            })
+            ret[item.dynamicName] = dynamicArr;
+          } else {
+            item = item as QueryParams;
+            ret[item.colection] = this.mapColection(item, data[item.colection]).data
+          }
         }
       }
     });
