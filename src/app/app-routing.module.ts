d45fe43bc5079aac28c0e6e21cfc53fde2198e20
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-09-12 20:50:46 +0200

    add page with transport and payment info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from "./contact/contact.component";
 import { BasketComponent } from "./basket/basket/basket.component";
 import { PageComponent } from "./components/page/page.component";
+import { TranspotPaymentComponent } from "./transpot-payment/transpot-payment.component";
 
 const routes: Routes = [
   { path: 'kosik', component: BasketComponent,},
   { path: 'kontakty', component: ContactComponent},
+  { path: 'doprava-a-platba', component: TranspotPaymentComponent},
   { path: '**', component: PageComponent },
 ];
 
