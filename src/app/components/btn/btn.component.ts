46e94d4f190cdde091fdbb49cc4d083138f6cc8e
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-09-12 20:44:02 +0200

    enable fragments routing in button

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, Input } from '@angular/core';
 import { BaseComponent } from "../base/base.component";
 import { BtnData } from "../../model/view";
 import { DOCUMENT } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-btn',
@@ -12,7 +13,20 @@ export class BtnComponent extends BaseComponent {
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
+    private router: Router,
   ) {
     super();
+  }
+
+  goTo(): void {
+    let fragments = this.data.link.split('#')
+    let fragment = '';
+    if(fragments.length>1) {
+      fragment=fragments[1];
+    }
+    console.log(fragments)
+    this.router.navigate(
+      [ '/', ...fragments[0].split('/')],
+      {fragment});
   }
 }
