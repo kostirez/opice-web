acb8346514a959a7d2b232adea5eee6e081c85bc
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-09-13 15:42:55 +0200

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { inject } from '@vercel/analytics';
+import { environment } from "./environments/environment";
+import { enableProdMode } from "@angular/core";
 
+if (environment.production) {
+  enableProdMode();
+}
+
+// Initialize Vercel Analytics
+inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
